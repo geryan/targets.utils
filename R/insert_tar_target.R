13c4ef9b102156fef092f3460482777178c0cae7
geryan <geryan@users.noreply.github.com>
2026-07-10 15:45:19 +1000

    remove `it` alias; it useless

--- R/insert_tar_target.R
+++ R/insert_tar_target.R
@@ -37,29 +37,3 @@ insert_tar_target <- function() {
 
   invisible(NULL)
 }
-
-#' Insert a tar_target() skeleton
-#'
-#' Alias for [insert_tar_target()].
-#'
-#' This function is intended for quick interactive use. It calls
-#' [insert_tar_target()] and inserts a `targets::tar_target()` skeleton at the
-#' current cursor position in RStudio.
-#'
-#' @return Invisibly returns `NULL`.
-#'
-#' @examples
-#' \dontrun{
-#' # In RStudio, place the cursor in an R script and run:
-#' it()
-#'
-#' # This is equivalent to:
-#' insert_tar_target()
-#' }
-#'
-#' @seealso [insert_tar_target()]
-#'
-#' @export
-it <- function() {
-  insert_tar_target()
-}
