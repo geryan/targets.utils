984c74268247c1ab79e062366e957a20fa41e7c3
geryan <geryan@users.noreply.github.com>
2026-07-10 11:43:42 +1000

    document `tl`

--- R/tl.R
+++ R/tl.R
@@ -1,5 +1,9 @@
 #' Targets load globals and everything
 #'
+#' Runs `targets::tar_load_globals` then `targets::tar_load_everything` to save
+#' you the drudgery of typing those thing out again and again and again and
+#' again and again et cetera ad nauseum.
+#'
 #' @returns Nothing
 #' @export
 #'
